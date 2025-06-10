7be054401f14635333d342683749eaaf78ce9fb4
joao <j.p.knight1999@gmail.com>
2025-06-09 21:15:17 -0400

    FEAT: adiciona biblioteca do vue-router e configura rotas de login e home

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,25 @@
-import { createApp } from 'vue'
-//@ts-ignore
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-import App from './App.vue'
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+import App from "./App.vue";
+import { router } from "./routes";
 
 const vuetify = createVuetify({
-  theme:{
-    defaultTheme:'anderson',
-    themes:{
-       anderson:{
-        dark:false,
+  theme: {
+    defaultTheme: "anderson",
+    themes: {
+      anderson: {
+        dark: false,
         colors: {
-          primary: "#FF0000"
-        }
-       }
-    }
+          primary: "#FF0000",
+        },
+      },
+    },
   },
-    components,
-    directives,
-  })
+  components,
+  directives,
+});
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(router).mount("#app");
